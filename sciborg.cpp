42691857a9ab184f0c29d9fef063fdf644f5a5f1
Bruno Teixeira <bruno.teixeira@zalando.de>
2013-06-28 17:21:34 +0200

    First implementation of callback setting. Must change a line in the V8 headers, don't like it at all.

--- sciborg.cpp
+++ sciborg.cpp
@@ -13,6 +13,9 @@
 
 v8::Handle<v8::Script> readFromFile(char* filename);
 
+v8::Isolate* isolate;
+v8::Handle<v8::Context> context;
+
 static int exit_app(GtkWidget*w, GdkEventAny*e, gpointer p) {
    gtk_main_quit();
    return w||e||p||1;	// Avoid warnings
@@ -22,22 +25,37 @@ static void testMe(const v8::FunctionCallbackInfo<v8::Value>& args) {
 	printf("ping!\n");
 }
 
+v8::Persistent<v8::Function> charAddedHandler;
+static void setHandler(const v8::FunctionCallbackInfo<v8::Value>& args) {
+	printf("setting handler!\n");
+	charAddedHandler = v8::Persistent<v8::Function>::New(v8::Handle<v8::Function>::Cast(args[0]));
+}
+
 ScintillaObject *sci;
 static void SEND_SCI_STYLESETBACK(const v8::FunctionCallbackInfo<v8::Value>& args) {
 	v8::Handle<v8::Value> arg = args[0];
 	SSM(SCI_STYLESETBACK, 0, arg->Int32Value());
 }
 
-v8::Handle<v8::Context> context;
 
-static void handleCA(GtkWidget *, gint /*wParam*/, SCNotification *lParam, void *data) {
-	if (lParam->nmhdr.code == SCN_PAINTED) {
+
+static void handleCA(GtkWidget *, gint /*wParam*/, SCNotification *notification, void *data) {
+	int code = notification->nmhdr.code;
+	if (code == SCN_PAINTED) {
 		/* Nothing, I guess */
-	} else if (lParam->nmhdr.code == SCN_CHARADDED) {
-		printf("HANDLING: %d\n", lParam->nmhdr.code);
-		v8::Handle<v8::Value> onSignalVal = context->Global()->Get(v8::String::New("onSignal"));
-		v8::Handle<v8::Function> onSignal = v8::Handle<v8::Function>::Cast(onSignalVal);
-		onSignal->Call(context->Global(), 0, NULL);
+	} else if (code == SCN_CHARADDED) {
+		//printf("HANDLING: %d\n", code);
+		if (!charAddedHandler.IsEmpty()) {
+			printf("we have a handler, calling it\n");
+			//v8::Handle<v8::Value> onSignalVal = context->Global()->Get(v8::String::New("onSignal"));
+			//charAddedHandler = v8::Handle<v8::Function>::Cast(onSignalVal);
+			v8::Local<v8::Object> global = context->Global();
+			//printf("empty global? %d\n", global.IsEmpty());
+			//printf("handler is fun? %d\n", charAddedHandler->IsFunction());
+			charAddedHandler->Call(global, 0, NULL);
+		} else {
+			printf("no handler\n");
+		}
 	}
 }
 
@@ -57,39 +75,15 @@ int main(int argc, char **argv) {
 	scintilla_set_id(sci, 0);
 	gtk_widget_set_usize(editor, 500, 300);
 
-	v8::Isolate* isolate = v8::Isolate::GetCurrent();
+	isolate = v8::Isolate::GetCurrent();
 	v8::HandleScope handle_scope(isolate);
 
 	v8::Handle<v8::ObjectTemplate> global = v8::ObjectTemplate::New();
 	global->Set(v8::String::New("testMe"), v8::FunctionTemplate::New(testMe));
 	global->Set(v8::String::New("SEND_SCI_STYLESETBACK"), v8::FunctionTemplate::New(SEND_SCI_STYLESETBACK));
-	/*
-	void Attach(GtkWidget *w, CommandHandler *object, const char *sigName="clicked") {
-		g_signal_connect(G_OBJECT(w), sigName, G_CALLBACK(Function), object);
-	}
-	static void Function(GtkWidget *, CommandHandler *object) {
-		object->PerformCommand(commandNumber);
-	}
-	*/
+	global->Set(v8::String::New("setHandler"), v8::FunctionTemplate::New(setHandler));
 
-	/* WORKS!!!!!! */
-	/* WORKS!!!!!! */
-	/* WORKS!!!!!! */
-	/* WORKS!!!!!! */
-	/* WORKS!!!!!! */
 	g_signal_connect(editor, SCINTILLA_NOTIFY, G_CALLBACK(handleCA), NULL);
-	/* WORKS!!!!!! */
-	/* WORKS!!!!!! */
-	/* WORKS!!!!!! */
-	/* WORKS!!!!!! */
-	/* WORKS!!!!!! */
-
-	/*
-	g_signal_connect(G_OBJECT(PWidget(wEditor)), SCINTILLA_NOTIFY,
-		               G_CALLBACK(NotifySignal), this);
-	g_signal_connect(G_OBJECT(PWidget(wOutput)), SCINTILLA_NOTIFY,
-		               G_CALLBACK(NotifySignal), this);
-	*/
 
 	context = v8::Context::New(isolate, NULL, global);
 	v8::Context::Scope context_scope(context);
@@ -120,7 +114,7 @@ v8::Handle<v8::Script> readFromFile(char* filename) {
 	fclose(f);
 
 	string[fsize] = 0;
-	printf("contents: %s\n", string);
+	//printf("contents: %s\n", string);
 	v8::Handle<v8::String> source = v8::String::New(string);
 	return v8::Script::Compile(source);
 }
